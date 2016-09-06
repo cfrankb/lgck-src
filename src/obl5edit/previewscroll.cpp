4238762e63439fb7266e94e446cfa6b1b9db7453
Francois Blanchette <fb2000x@yahoo.ca>
2016-09-05 23:04:04 -0400

    added missing copyright notices (GPL3)

--- src/obl5edit/previewscroll.cpp
+++ src/obl5edit/previewscroll.cpp
@@ -1,3 +1,21 @@
+/*
+    LGCK Builder package
+    Copyright (C) 2005, 2016  Francois Blanchette
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "previewscroll.h"
 #include "previewwidget.h"
 #include <QScrollBar>
@@ -38,7 +56,6 @@ void CPreviewScroll::paintEvent(QPaintEvent *event)
 
 void CPreviewScroll::updateScrollbars()
 {
-   // qDebug("CFrameScroll::updateScrollbars()");
     CPreviewWidget *widget = m_widget;
     QSize sz = size();
     CFrame *frame = widget->m_frame;
@@ -102,10 +119,6 @@ void CPreviewScroll::mousePressEvent(QMouseEvent * event)
         if (m_mouse.rButton) {
             zoom = std::max(1, --zoom);
         }
-        //qDebug("zoom:%d", m_zoom);
-        //changeFrame(m_frame);
-      //  m_scene->update();
-        qDebug("dseewewwe");
         emit zoomChanged(zoom);
         update();
     }
