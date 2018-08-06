6afde8baf75bc4adc7705fe84b5b6223d1e54488
Frank B <cfrankb.2000@gmail.com>
2018-08-05 21:54:20 -0400

    blah

--- src/tools/buildcount.py
+++ src/tools/buildcount.py
@@ -1,9 +1,12 @@
+import uuid
+import re
 import os
 from os.path import join
 
 path = os.path.dirname(os.path.realpath(__file__))
 DATA = join(path, 'buildcount.dat')
 
+build_no = 0
 with open(DATA, 'r') as s:
     build_no = s.read().strip()
     
@@ -21,3 +24,27 @@ print(build_no)
 target = "../shared/ss_build.h"
 with open(target, 'wb') as t:
     t.write("#define SS_BUILD {}".format(build_no))
+
+with open('../shared/ss_version.h') as s:
+    data = s.read()
+ver = data.replace('#define SS_LGCK_VERSION 0x', '').strip()
+ver = re.findall('..?', ver)[0:2] + [build_no]
+print ver
+
+version = '.'.join([str(int(x)) for x in ver])
+
+with open('../build/lgck-setup.aip', 'rb') as s:
+    in_data = s.read().split('\r\n')
+
+out_data = []
+for line in in_data:
+    if '<ROW Property="ProductVersion" Value' in line:
+        line = '<ROW Property="ProductVersion" Value="{version}" Type="32"/>'.format(version=version)
+    elif '<ROW Property="ProductCode" Value="' in line:
+        line = '<ROW Property="ProductCode" Value="1033:{{{}}} " Type="16"/>'.format(str(uuid.uuid4()))
+    elif '<ROW Property="UpgradeCode" Value="' in line:
+        line= '<ROW Property="UpgradeCode" Value="{{{}}}"/>'.format(str(uuid.uuid4()))
+    out_data.append(line)
+
+with open('../build/build.aip', 'wb') as t:
+    t.write('\r\n'.join(out_data))
--- src/tools/versionman.py
+++ src/tools/versionman.py
@@ -3,6 +3,8 @@
 import re
 import datetime
 import json
+import uuid
+import re
 
 TEMPL = r'''#ifndef VERSION_H
 #define VERSION_H
@@ -58,3 +60,31 @@ for f in files:
             int_file_desc=int_file_desc,
             c='\xa9')
         )
+
+
+
+#build_no = 100
+
+with open('../shared/ss_version.h') as s:
+    data = s.read()
+ver = data.replace('#define SS_LGCK_VERSION 0x', '').strip()
+ver = re.findall('..?', ver)[0:2] + [build_no]
+print ver
+
+version = '.'.join([str(int(x)) for x in ver])
+
+with open('../build/lgck-setup.aip', 'rb') as s:
+    in_data = s.read().split('\r\n')
+
+out_data = []
+for line in in_data:
+    if '<ROW Property="ProductVersion" Value' in line:
+        line = '<ROW Property="ProductVersion" Value="{version}" Type="32"/>'.format(version='0.6.100')
+    elif '<ROW Property="ProductCode" Value="' in line:
+        line = '<ROW Property="ProductCode" Value="1033:{{{}}} " Type="16"/>'.format(str(uuid.uuid4()))
+    elif '<ROW Property="UpgradeCode" Value="' in line:
+        line= '<ROW Property="UpgradeCode" Value="{{{}}}"/>'.format(str(uuid.uuid4()))
+    out_data.append(line)
+    
+with open('../build/test.aip', 'wb') as t:
+    t.write('\r\n'.join(out_data))
