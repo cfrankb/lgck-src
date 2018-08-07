75c257864ac6a0c86392814c69ac09b351d85e64
Frank B <cfrankb.2000@gmail.com>
2018-08-06 21:33:32 -0400

    Fix version tools

--- src/tools/buildcount.py
+++ src/tools/buildcount.py
@@ -9,7 +9,7 @@ DATA = join(path, 'buildcount.dat')
 build_no = 0
 with open(DATA, 'r') as s:
     build_no = s.read().strip()
-    
+
 if not build_no:
     build_no = 0
 
@@ -21,30 +21,18 @@ with open(DATA, 'w') as t:
 os.chdir(path)
 
 print(build_no)
-target = "../shared/ss_build.h"
-with open(target, 'wb') as t:
-    t.write("#define SS_BUILD {}".format(build_no))
 
 with open('../shared/ss_version.h') as s:
     data = s.read()
 ver = data.replace('#define SS_LGCK_VERSION 0x', '').strip()
-ver = re.findall('..?', ver)[0:2] + [build_no]
-print ver
+ver = re.findall('..?', ver)
 
-version = '.'.join([str(int(x)) for x in ver])
+version = '.'.join([str(int(x, 16)) for x in ver])
+build = '{version} build {build_no}'.format(version=version, build_no=build_no)
+print (build)
 
-with open('../build/lgck-setup.aip', 'rb') as s:
-    in_data = s.read().split('\r\n')
-
-out_data = []
-for line in in_data:
-    if '<ROW Property="ProductVersion" Value' in line:
-        line = '<ROW Property="ProductVersion" Value="{version}" Type="32"/>'.format(version=version)
-    elif '<ROW Property="ProductCode" Value="' in line:
-        line = '<ROW Property="ProductCode" Value="1033:{{{}}} " Type="16"/>'.format(str(uuid.uuid4()))
-    elif '<ROW Property="UpgradeCode" Value="' in line:
-        line= '<ROW Property="UpgradeCode" Value="{{{}}}"/>'.format(str(uuid.uuid4()))
-    out_data.append(line)
-
-with open('../build/build.aip', 'wb') as t:
-    t.write('\r\n'.join(out_data))
+target = "../shared/ss_build.h"
+with open(target, 'wb') as t:
+    t.write('#define SS_BUILD "{}"\n'.format(build_no))
+    t.write('#define SS_BUILD_STR "{}"'.format(build))
+    
--- src/tools/versionman.py
+++ src/tools/versionman.py
@@ -30,15 +30,11 @@ TEMPL = r'''#ifndef VERSION_H
 
 with open('../shared/ss_version.h') as s:
     data = s.read()
-    
+
 ver = data.replace('#define SS_LGCK_VERSION 0x', '').strip() # 0x00060008
-
 ver = re.findall('..?', ver)
-
-
 now = datetime.datetime.now()
-
-version = ','.join([str(int(x)) for x in ver])
+version = ','.join([str(int(x, 16)) for x in ver])
 version_str = version.replace(',','.')
 
 with open("versionman.json") as s:
@@ -58,33 +54,4 @@ for f in files:
             app_exe=app_exe,
             file_desc=file_desc,
             int_file_desc=int_file_desc,
-            c='\xa9')
-        )
-
-
-
-#build_no = 100
-
-with open('../shared/ss_version.h') as s:
-    data = s.read()
-ver = data.replace('#define SS_LGCK_VERSION 0x', '').strip()
-ver = re.findall('..?', ver)[0:2] + [build_no]
-print ver
-
-version = '.'.join([str(int(x)) for x in ver])
-
-with open('../build/lgck-setup.aip', 'rb') as s:
-    in_data = s.read().split('\r\n')
-
-out_data = []
-for line in in_data:
-    if '<ROW Property="ProductVersion" Value' in line:
-        line = '<ROW Property="ProductVersion" Value="{version}" Type="32"/>'.format(version='0.6.100')
-    elif '<ROW Property="ProductCode" Value="' in line:
-        line = '<ROW Property="ProductCode" Value="1033:{{{}}} " Type="16"/>'.format(str(uuid.uuid4()))
-    elif '<ROW Property="UpgradeCode" Value="' in line:
-        line= '<ROW Property="UpgradeCode" Value="{{{}}}"/>'.format(str(uuid.uuid4()))
-    out_data.append(line)
-    
-with open('../build/test.aip', 'wb') as t:
-    t.write('\r\n'.join(out_data))
+            c='\xa9'))
