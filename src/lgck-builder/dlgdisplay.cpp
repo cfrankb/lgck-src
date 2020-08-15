b4850cbe31b1ca35173ed1db9c3e45f12bcdc3d8
Frankb <cfrankb.2000@gmail.com>
2020-08-15 02:51:03 -0400

    missing license on source files

--- src/lgck-builder/dlgdisplay.cpp
+++ src/lgck-builder/dlgdisplay.cpp
@@ -1,3 +1,20 @@
+/*
+    LGCK Builder Runtime
+    Copyright (C) 1999, 2020  Francois Blanchette
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "dlgdisplay.h"
 #include "ui_dlgdisplay.h"
 
@@ -98,8 +115,7 @@ void CDlgDisplay::load(CDisplay & d)
     ui->btnShadowColor->setBuddy(ui->eShadowColor);
     ui->eShadowColor->setText(QString::asprintf("%.2X%.2X%.2X", d.shadowR(), d.shadowG(), d.shadowB()));
     ui->sShadowAlpha->setBuddy(ui->eShadowAlpha);
-    ui->sShadowAlpha->setSliderPosition(d.shadowA(
-                                            ));
+    ui->sShadowAlpha->setSliderPosition(d.shadowA());
     ui->eShadowX->setText(STR(d.shadowX()));
     ui->eShadowY->setText(STR(d.shadowY()));
     ui->cShadowVisible->setChecked(d.shadow());
